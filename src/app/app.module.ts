f30fca72918a8775a09505ac5fb157d31f65c4e2
richard-romanowski <richard.romanowski@gmail.com>
2020-06-21 20:55:58 -0500

    initial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,36 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { translations, translationChunksConfig } from '@spartacus/assets';
+import { B2cStorefrontModule } from '@spartacus/storefront';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    B2cStorefrontModule.withConfig({
+      backend: {
+        occ: {
+          baseUrl: 'https://spartacus.c39j2-walkersde1-d3-public.model-t.cc.commerce.ondemand.com/',
+          prefix: '/rest/v2/'
+        }
+      },
+      context: {
+        currency: ['USD'],
+        language: ['en'],
+        baseSite: ['electronics-spa']
+      },
+      i18n: {
+        resources: translations,
+        chunks: translationChunksConfig,
+        fallbackLang: 'en'
+      },
+      features: {
+        level: '2.0'
+      }
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
