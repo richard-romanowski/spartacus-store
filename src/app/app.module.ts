92faebf787ba230e0fc223af78abdbd22de2246b
richard-romanowski <richard.romanowski@gmail.com>
2020-06-22 21:11:03 -0500

    end of unit 3, automatic configuration

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -19,9 +19,6 @@ import { B2cStorefrontModule } from '@spartacus/storefront';
         }
       },
       context: {
-        // baseSite: ['electronics-spa'],
-        // customParam: ['test'],
-        // urlParameters: ['customParam', 'baseSite', 'language', 'currency']
       },
       i18n: {
         resources: translations,
