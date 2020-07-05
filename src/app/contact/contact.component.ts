35ef92da1e58d2c23c1f166bb7ebb5e1ee3a23c4
richard-romanowski <richard.romanowski@gmail.com>
2020-07-05 16:12:49 -0500

    add programatic link

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { RoutingService } from '@spartacus/core';
 
 @Component({
   selector: 'app-contact',
@@ -7,9 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactComponent implements OnInit {
 
-  constructor() { }
+  constructor(private routingService: RoutingService) { }
 
   ngOnInit(): void {
   }
+
+  goToRandomProduct() {
+    this.routingService.go({
+      cxRoute: 'product',
+      params: { code: '1981414', nameForUrl: 'pl60-pink' }
+     });
+  }
+
 
 }
