1993de675462219e3bea67680ac75076bfe3673c
richard-romanowski <richard.romanowski@gmail.com>
2020-07-05 23:27:44 -0500

    ignore empty results

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { RoutingService, ProductSearchService } from '@spartacus/core';
-
+import { filter } from 'rxjs/operators'
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
@@ -15,7 +15,11 @@ export class ContactComponent implements OnInit {
 
   goToRandomProduct() {
     this.productSearchService.search('cam');
-    this.productSearchService.getResults().subscribe(console.log);
+    this.productSearchService.getResults().
+      pipe(
+        filter(res => res.hasOwnProperty('products'))
+      ).
+      subscribe(console.log);
     // this.routingService.go({
     //   cxRoute: 'product',
     //   params: { code: '1981414', nameForUrl: 'pl60-pink' }
